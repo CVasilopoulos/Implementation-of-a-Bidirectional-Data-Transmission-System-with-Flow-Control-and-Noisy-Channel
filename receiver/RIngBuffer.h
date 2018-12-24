9598ce824c28e6751a19180354c53299d5687565
Christos Vasilopoulos <ch.vasilo@gmail.com>
2018-12-24 10:13:25 +0200

    Add files via upload

---
+++ receiver/RIngBuffer.h
@@ -0,0 +1,42 @@
+
+#ifndef RINGBUFFER_H_
+#define RINGBUFFER_H_
+
+#define RING_BUF_SIZE 		2048
+#define RING_BUF_SIZE_MASK (RING_BUF_SIZE-1)
+#define HWM_BUF_SIZE 		153
+#define LWM_BUF_SIZE 		51
+
+typedef unsigned short rb_size_t; // caution this holds indices e.t.c (max limit 65535)
+
+typedef struct
+{
+	unsigned char buf[RING_BUF_SIZE];
+	volatile rb_size_t wr_idx;
+	volatile rb_size_t rd_idx;
+} ring_buffer_t;
+
+
+rb_size_t RingBufferNumItems(ring_buffer_t *rb);
+
+rb_size_t RingBufferFreeItems(ring_buffer_t *rb);
+
+unsigned char ring_buffer_hwm_reached(ring_buffer_t *rb);
+
+unsigned char ring_buffer_lwm_reached(ring_buffer_t *rb);
+
+void RingBufferInit(ring_buffer_t *rb);
+
+unsigned char RingBufferWrite(ring_buffer_t *rb, const unsigned char data);
+
+unsigned char RingBufferRead(ring_buffer_t *rb,  unsigned char *data);
+
+unsigned char RingBufferWriteArray(ring_buffer_t *rb, const unsigned char *data, rb_size_t len);
+
+rb_size_t RingBufferReadArray(ring_buffer_t *rb, unsigned char *data, rb_size_t len);
+
+extern ring_buffer_t rb_tx;
+extern ring_buffer_t rb_rx;
+extern ring_buffer_t ParseRecvBuffer;
+
+#endif /* RINGBUFFER_H_ */
---
+++ receiver/Timer.h
@@ -0,0 +1,43 @@
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include "xparameters.h"
+#include "xtmrctr.h"
+#include "xil_exception.h"
+#include "xil_printf.h"
+
+#define TIMER_CNTR_0		 		XPAR_TIMER_0_DEVICE_ID
+#define TIMER_CNTR_1	 			XPAR_TIMER_1_DEVICE_ID
+#define TIMER_CNTR_2	 			XPAR_TIMER_2_DEVICE_ID
+#define TMRCTR_INTERRUPT_ID_0		XPAR_INTC_0_TMRCTR_0_VEC_ID
+#define TMRCTR_INTERRUPT_ID_1		XPAR_INTC_0_TMRCTR_1_VEC_ID
+#define TMRCTR_INTERRUPT_ID_2		XPAR_INTC_0_TMRCTR_2_VEC_ID
+
+/*
+ * The following constant is used to set the reset value of the timer counter,
+ * making this number larger reduces the amount of time this example consumes
+ * because it is the value the timer counter is loaded with when it is started
+ */
+#define RESET_0_VALUE	 		0xF1194D81 // Resend
+#define RESET_1_VALUE	 		0xFFF85EE1 // RTS checking
+#define RESET_2_VALUE	 		0xFFF85EE1 // Scheduler
+
+#define LED_ADDRESS				XPAR_LEDS_BASEADDR
+
+// 0xFE8287C1 -> 0.25 sec
+// 0xFF676981 -> 0.1 sec
+// 0xFFF85EE1 -> 5 ms
+// 0xFFB3B4C1 -> 50 ms
+// 0xFD050F81 -> 500 ms // PERIPOU 43 PAYLOAD_SIZE
+// 0xFBD3E281 -> 700 ms
+
+
+void Timers_Init();
+
+XTmrCtr TimerCounterInst_0;
+XTmrCtr TimerCounterInst_1;
+XTmrCtr TimerCounterInst_2;
+
+
+#endif /* TIMER_H_ */
